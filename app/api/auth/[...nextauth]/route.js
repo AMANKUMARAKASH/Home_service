fb8ccb428e8e39a0690c9aedf3777f62579d6a0b
AMANKUMARAKASH <89243224+AMANKUMARAKASH@users.noreply.github.com>
2024-03-22 13:20:58 +0530

    new changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,7 @@ export const authOptions = {
         authorization: { params: { scope: "openid email profile" } },
         idToken: true,
         clientId: process.env.DESCOPE_CLIENT_ID,
-        clientSecret: process.env.DESCOPE_SCERET,
+        clientSecret: process.env.DESCOPE_SECRET,
         checks: ["pkce", "state"],
         profile(profile) {
             return {
@@ -22,62 +22,7 @@ export const authOptions = {
             }
         },
     }],
-    callbacks: {
-        async jwt({token, account, profile}) {
-            if (account) {
-                return {
-                    ...token,
-                    access_token: account.access_token,
-                    expires_at: Math.floor(Date.now() / 1000 + account.expires_in),
-                    refresh_token: account.refresh_token,
-                    profile: {
-                      name: profile?.name,
-                      email: profile?.email,
-                      image: profile?.picture,
-                  },
-                }
-            } else if (Date.now() < token.expires_at * 1000) {
-                return token
-            } else {
-                try {
-                    const response = await fetch("https://api.descope.com/oauth2/v1/token", {
-                        headers: {"Content-Type": "application/x-www-form-urlencoded"},
-                        body: new URLSearchParams({
-                            client_id:  process.env.DESCOPE_CLIENT_ID,
-                            client_secret: process.env.DESCOPE_SCERET,
-                            grant_type: "refresh_token",
-                            refresh_token: token.refresh_token,
-                        }),
-                        method: "POST",
-                    })
     
-                    const tokens = await response.json()
-    
-                    if (!response.ok) throw tokens
-    
-                    return {
-                        ...token,
-                        access_token: tokens.access_token,
-                        expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
-                        refresh_token: tokens.refresh_token ?? token.refresh_token,
-                    }
-                } catch (error) {
-                    console.error("Error refreshing access token", error)
-                    return {...token, error: "RefreshAccessTokenError"}
-                }
-            }
-        },
-    
-        async session({session, token}) {
-            if (token.profile) {
-              session.user = token.profile;
-            }
-    
-            session.error = token.error
-            session.accessToken = token.access_token
-            return session
-        },
-    },
 
     secret: process.env.NEXTAUTH_SECRET 
     
