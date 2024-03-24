3a86fb5341fbeb04925df027f46925aea6a8bfa6
AMANKUMARAKASH <89243224+AMANKUMARAKASH@users.noreply.github.com>
2024-03-24 09:09:52 +0530

    adding footer

---
+++ app/_components/Footer.jsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+
+        <div className="border bg-gray-20" />
+        <h3 className="font-bold text-[18px] text-center text-gray-30">2024 Home_service | <span className='text-primary'>All rights reserved</span></h3>
+      </div>
+    </footer>
+  )
+}
+
+
+
+
+export default Footer
